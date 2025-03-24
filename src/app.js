688f6243644134a83274fb4c80baf85ebad16482
krishcodespace <kanokakadiya47@gmail.com>
2025-03-24 21:05:08 +0530

    Multiple-Route-Handlers

--- src/app.js
+++ src/app.js
@@ -1,45 +1,37 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/demo/:id', (req, res) => {
-  res.send("this is get send api call")
-})
+app.use(
+  "/user",
+  (req, res, next) => {
+    // this second callback function know as Route Handler.
+    console.log("Handling the route user");
+    next();
+    // res.send("Response!!");
+  },
+  (req, res, next) => {
+    console.log("Handling the route user2!!");
+    next();
+    // res.send("2nd Response!!");
+  },
+  (req, res, next) => {
+    console.log("Handling the route user3!!");
+    next();
+    // res.send("3nd Response!!");
+  },
+  (req, res, next) => {
+    console.log("Handling the route user4!!");
+    // res.send("4nd Response!!");
+    next();
+  }
+);
 
-// you also use regex as route
-app.get('/*a/' ,(req, res) => {
-  res.send({"regexValue":"this is reges example"})
-})
+// you can also keep rout handler in one arry it's work same as normal Rout Handlers
+// warpping Rout Handlers insdie array not make any impact
+// app.use("/data",[()=>{},()=>{}])
 
-app.get('/a+bc+d/' ,(req, res) => {
-  res.send({"/a+bc+d/":"this is reges example"})
-})
-
-app.get('/ab?d/' ,(req, res) => {
-  res.send({"/ab?d":"this is reges example"})
-})
-
-app.use("/hello",(req, res) => {
-  res.send("you got response from hello route")
-});
-
-// how get query from request
-app.get('/users', (req, res) => {
-  console.log(req.query);
-  res.send("show exmpale how we get query form request")
-})
-
-// how get params from request
-app.get('/users/:id', (req, res) => {
-  console.log(req.params);
-  res.send("show exmpale how we get params form request")
-})
-
-// app.use("/",(req, res) => {
-//   res.send("Hello this is my server you have got response from it.")
-// });
 
 app.listen(3676, () => {
-    console.log('Server run Successfully on 3676')
+  console.log("Server run Successfully on 3676");
 });
-
