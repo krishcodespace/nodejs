13f8381d3f23bbd5676d26415613f1e480976600
krishcodespace <kanokakadiya47@gmail.com>
2025-03-28 23:53:08 +0530

    GET APis for allusers & userByEmail

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-const express = require("express");
 require("./config/database");
+const express = require("express");
+const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
-const app = express();
 
 app.use(express.json());
 
@@ -10,12 +10,49 @@ app.post("/signup", async (req, res, next) => {
   // in most monggos functio is retun promise so we add async/await
   const user = await new User(req.body);
   try {
-    await user.save();
-    res.send("User Added successfully!");
+    const isUserExist = await User.find({ emailId : req.body.emailId});
+    if(isUserExist.length === 0){
+      await user.save();
+      res.send("User Added successfully!");
+    }else{
+      res.status(200).send("User Already exist with these email id!");
+    }
   } catch (err) {
     res.status(400).send("Error saving user:" + err.message);
   }
+});
 
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    // const users = await User.find({ emailId: userEmail });
+    const users = await User.findOne();
+    // const users = await User.findOne({ emailId: userEmail }); // incase two use with same emailid it will give you first one 
+    if (users.length === 0) {
+      res.status(400).send("User not found");
+    } else {
+      res.status(200).send(users);
+    }
+  } catch (error) {
+    res.status(404).send("User not found with this email id");
+  }
+});
+
+//Feed API - GET /feed - get all use from database
+app.get("/feed", async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+      // res.status(200).send(allUsers);
+      res.send({
+        "data":allUsers,
+        "status":200,
+        "message":"you got users!",
+        "errorMessage": null
+      })
+
+  } catch (error) {
+    res.status(404).send("Something went wrong");
+  }
 });
 
 connectDB()
