055fad1efe30bac0c24f44b279a49d2a94a2b358
krishcodespace <kanokakadiya47@gmail.com>
2025-03-29 01:00:05 +0530

    CREATE API for DELETE USER & GETUSERBYID

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const User = require("./models/user");
 
 app.use(express.json());
 
+//ADD NEW USER - POST /signup
 app.post("/signup", async (req, res, next) => {
   // in most monggos functio is retun promise so we add async/await
   const user = await new User(req.body);
@@ -25,8 +26,7 @@ app.post("/signup", async (req, res, next) => {
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
   try {
-    // const users = await User.find({ emailId: userEmail });
-    const users = await User.findOne();
+    const users = await User.find({ emailId: userEmail });
     // const users = await User.findOne({ emailId: userEmail }); // incase two use with same emailid it will give you first one 
     if (users.length === 0) {
       res.status(400).send("User not found");
@@ -35,6 +35,23 @@ app.get("/user", async (req, res) => {
     }
   } catch (error) {
     res.status(404).send("User not found with this email id");
+  }
+});
+
+app.get("/getuser", async (req, res) => {
+  const userId = req.query.userId;
+  console.log("userID", req.query);
+  try {
+    const users = await User.find({ _id: userId });
+    console.log(users);
+    // const users = await User.findOne({ emailId: userEmail }); // incase two use with same emailid it will give you first one 
+    if (users.length > 0) {
+      res.status(200).send(users);
+    } else {
+      res.status(400).send("User not found");
+    }
+  } catch (error) {
+    res.status(404).send("User not found with this id");
   }
 });
 
@@ -54,6 +71,39 @@ app.get("/feed", async (req, res) => {
     res.status(404).send("Something went wrong");
   }
 });
+
+//DELETe SPI - DELETE /deleteuser 
+app.delete("/deleteuser", async (req, res) => {
+   const userId = req.query.userId;
+  console.log("userId",userId);
+
+   try {
+      const isValidUserID = await User.find({_id: userId});
+      console.log(isValidUserID)
+      if(isValidUserID.length > 0 ){
+        const userdelete = await User.findByIdAndDelete({_id: userId});
+        // const userdelete = await User.findByIdAndDelete(userId);
+        res.send("user delete success fully!")
+      }else{
+        res.status(400).send("user not found with these id!")
+      }
+   } catch (error) {
+    res.status(400).send("Something went wrong!");
+   }
+});
+
+//Update api - PATCH /updateuser
+app.patch("/update", async(req, res) => {
+ const userId = req.body._id;
+ const userData = req.body;
+
+ try {
+  const upateUser = await User.findByIdAndUpdate({ _id : userId}, userData);
+  res.send("User update successfully!") 
+ } catch (error) {
+  res.status(500).send("something went wrong!");
+ }
+})
 
 connectDB()
   .then(() => {
