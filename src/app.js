40d826d9e0791b26e3cd6a504e65a0aec5013c90
krishcodespace <kanokakadiya47@gmail.com>
2025-03-28 22:56:17 +0530

    Dynamic data for signup Api

--- src/app.js
+++ src/app.js
@@ -4,36 +4,25 @@ const connectDB = require("./config/database");
 const User = require("./models/user");
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async (req, res, next) => {
-  const userObj = {
-    firstName: "krishn",
-    lastName: "ofjiefwjfie",
-    email: "Krishn@gmail.com",
-  };
-
-  // creating a new instace of user model
-  // const user = new User(userObj);
-
-//////////////////////////OR////////////////////////////
-
-  const user = new User({
-    firstName: "krishn",
-    lastName: "ofjiefwjfie",
-    email: "Krishn@gmail.com",
-    password: "jfoijefiw@546454",
-  });
-
   // in most monggos functio is retun promise so we add async/await
-  await user.save();
+  const user = await new User(req.body);
+  try {
+    await user.save();
+    res.send("User Added successfully!");
+  } catch (err) {
+    res.status(400).send("Error saving user:" + err.message);
+  }
 
-  res.status(200).send("User Add successfully.")
 });
 
 connectDB()
   .then(() => {
     console.log("Database Connect succesfully!");
-    app.listen(3676, () => {
-      console.log("Server run Successfully on 3676");
+    app.listen(8000, () => {
+      console.log("Server run Successfully on 8000");
     });
   })
   .catch((err) => {
