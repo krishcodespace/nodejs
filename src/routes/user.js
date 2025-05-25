a567febbebbebb432fcfcd90dbca529a6329695e
krishcodespace <kanokakadiya47@gmail.com>
2025-05-25 08:35:37 +0530

    Update user/connections Api

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,7 +4,7 @@ const ConnectionRequest = require("../models/connectionRequest");
 const User = require('../models/user')
 
 const userRouter = express.Router();
-
+const USER_SAFE_DATA = "firstName lastName about age skills gender photoUrl"
 userRouter.get('/user/requests/recevied',userAuth, async(req, res) => {
 
     const loggedInuser = req.user;
@@ -20,6 +20,37 @@ userRouter.get('/user/requests/recevied',userAuth, async(req, res) => {
         userRequests
     });
 
+})
+
+userRouter.get('/user/connections', userAuth, async(req, res) => {
+    try{
+        const loggedInuser = req.user;
+        const userRequests = await ConnectionRequest.find({
+            $or:[
+                {  toUserId: loggedInuser._id, status: "accepted"},
+                {  fromUserId: loggedInuser._id, status: "accepted"}
+            ],
+        })
+        .populate("fromUserId",USER_SAFE_DATA)
+        .populate("toUserId", USER_SAFE_DATA);
+        // }).populate("fromUserId",["firstName", "lastName"]); // second way to write feilds
+
+        // const data = userRequests.map((row) => row.fromUserId);
+        const data = userRequests.map((row) => {
+            if(row.fromUserId._id.toString() === loggedInuser._id.toString()){
+              return row.toUserId;
+            }else{
+             return row.fromUserId;
+            }
+        });
+        res.status(200).json({
+            message: loggedInuser.firstName + " " + "Connection Request got successfull!",
+            data:
+            data
+        });
+    }catch(error){
+        res.status(400).json({ message: error})
+    }
 })
 
 module.exports = userRouter;
